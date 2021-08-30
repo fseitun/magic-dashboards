31c61c6457daacaf3321e0afd2654640c736e1cd
fseitun <fseitun@gmail.com>
2021-08-30 18:37:49 -0300

    cards casi listos (solo falta formato fino)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-
 import { Dashboard } from 'components/Dashboard';
+import dotenv from 'dotenv';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
+
+dotenv.config();
 
 function App() {
-  return <Dashboard />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Dashboard />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
